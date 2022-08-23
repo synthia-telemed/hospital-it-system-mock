759a40821de934acfa124bf8b5741c096bdf6355
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-08-23 13:23:28 +0700

    add assertDoctorPassword query

--- src/doctor/doctor.resolver.ts
+++ src/doctor/doctor.resolver.ts
@@ -1,5 +1,6 @@
 import { Args, Info, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { GraphQLResolveInfo } from 'graphql'
+import * as bcrypt from 'bcrypt'
 import { BaseResolver } from 'src/base.resolver'
 import { Doctor, DoctorCreateInput, DoctorWhereInput, FindManyDoctorArgs } from 'src/@generated/doctor'
 import { PrismaService } from 'src/prisma.service'
@@ -20,6 +21,16 @@ export class DoctorResolver extends BaseResolver {
 	async doctors(@Args() condition: FindManyDoctorArgs, @Info() info: GraphQLResolveInfo): Promise<Doctor[]> {
 		this.cleanEmptyWhereField(condition)
 		return this.prismaService.doctor.findMany({ ...condition, ...this.getPrismaSelect(info) })
+	}
+
+	@Query(_returns => Boolean)
+	async assertDoctorPassword(
+		@Args('username') username: string,
+		@Args('password') password: string
+	): Promise<boolean> {
+		const doctor = await this.prismaService.doctor.findFirst({ where: { username }, select: { password: true } })
+		if (!doctor) return false
+		return bcrypt.compare(password, doctor.password)
 	}
 
 	@Mutation(_returns => Doctor)
