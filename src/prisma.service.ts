74695df22cfd61c242ab44de4f862ecdbc727f49
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-08-27 09:57:08 +0700

    change log level

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -5,7 +5,7 @@ import { PrismaClient } from '@prisma/client'
 export class PrismaService extends PrismaClient implements OnModuleInit {
 	constructor() {
 		super({
-			log: ['query', 'info', 'warn', 'error'],
+			log: ['warn', 'error'],
 		})
 	}
 	async onModuleInit() {
